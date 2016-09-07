0405afca262f6cbe0b657a8077e53d0f9ecb2285
jpbeaudet <jpbeaudet84@gmail.com>
2016-09-07 19:06:59 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # appHero
 App of which you are the hero. Small story telling game/editor for my nephew !
+
+## Here will go instruction for usages
---
+++ config.js
@@ -0,0 +1,13 @@
+// Author: Jean-Philippe Beaudet @ S3R3NITY Technology || Mathieu Fanoni
+// 
+// Apphero app dont vous etes le hero
+// Version : 0.0.1
+// License: 
+//
+var Config = {
+    node_web_server_host: "localhost",
+    node_web_server_port: 8080,
+    mongodb_database: 'apphero',
+    mongodb_host : "localhost"
+};
+module.exports = Config;
